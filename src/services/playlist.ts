bb60fcb3e004b7012cb07cd3f2c8a894fe7d7798
Niels <56843328+Nierot@users.noreply.github.com>
2021-12-26 18:57:16 +0100

    ✨ Add Playlist:getAllFromUser()

--- src/services/playlist.ts
+++ src/services/playlist.ts
@@ -2,6 +2,9 @@ import { SupabaseClient } from "@supabase/supabase-js";
 
 export type PlaylistType = {
   id: number
+  createdAt: Date
+  name: string
+  user: string
 }
 
 export default class Playlist {
@@ -27,6 +30,50 @@ export default class Playlist {
 
     return {
       id: data[0].id,
+      createdAt: new Date(data[0].created_at),
+      name: data[0].name,
+      user: data[0].user,
     }
+  }
+
+  async getAllFromUser(): Promise<PlaylistType[]> {
+    const uid = this.client.auth.user()?.id
+
+    if (!uid) {
+      throw new Error('User not found')
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const { data, error }: { data: any[] | null, error: any } = await this.client
+      .from('playlist')
+      .select()
+      .match({ user: uid })
+
+    if (error !== null) {
+      throw error
+    }
+
+    if (data === null) {
+      throw new Error('Found no playlists, query is null')
+    }
+
+    if (data.length === 0) {
+      return []
+    }
+
+    const playlists: PlaylistType[] = []
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    data.forEach((playlist: any) => {
+      playlists.push({
+        id: playlist.id,
+        createdAt: new Date(playlist.created_at),
+        name: playlist.name,
+        user: playlist.user,
+      })
+    })
+
+    return playlists
+    
   }
 }
