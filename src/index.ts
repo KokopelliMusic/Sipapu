a944d55b53abc00d4518a53201ea603ef91f6600
Niels <56843328+Nierot@users.noreply.github.com>
2021-12-28 18:35:56 +0100

    ♻️ Remove AnonymousSipapu

--- src/index.ts
+++ src/index.ts
@@ -4,38 +4,106 @@ import Session from './services/session'
 import Song from './services/song'
 import { settings } from './settings'
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface ISipapu {
-  Session: Session
-  Playlist: Playlist
-  Song: Song
-}
-
 /**
  * A Sipapu client.
- * This is the authenticated version, it requires a valid access token to instanciate
- * The usual workflow to use this client is:
- *  - Create an {@link AnonymousSipapu} client
- *  - Authenticate the client
- *  - Use the client to query data
+ * This client is anonymous as long as signIn() or singUp() is not called
  */
-export class Sipapu implements ISipapu {
-  private client: SupabaseClient
+export class Sipapu {
+  client: SupabaseClient
   Session: Session
   Playlist: Playlist
   Song: Song
 
-  constructor(apiKey: string) {
+  /**
+   * Constructor for the Sipapu client
+   */
+  constructor() {
+
     const options: SupabaseClientOptions = {
       headers: {
-        apiKey
-      }
+        // ja ik ook leesbare code schrijven 
+        // apiKey: apiKey ?? (token ?? '')
+      },
+      autoRefreshToken: true,
+      persistSession: true
     }
 
     this.client = createClient(settings.supabaseUrl, settings.supabaseKey, options)
     this.Session = new Session(this.client, this)
     this.Playlist = new Playlist(this.client, this)
     this.Song = new Song(this.client, this)
+  }
+
+  isLoggedIn(): boolean {
+    return this.client.auth.session() !== null
+  }
+
+  /**
+   * Sign up an user for Kokopelli and sets the api token to the new one
+   * @param email string The email of the user 
+   * @param password string The password of the user 
+   * @param username string The username of the user
+   * @throws {@link Error} if the email or password are incorrect
+   * @throws {@link ApiError} in the case Supabase screws up
+   */
+   async signUp(email: string, password: string, username: string): Promise<void> {
+    const { user, session, error } = await this.client.auth.signUp(
+      {
+        email,
+        password,
+      },
+      {
+        data: {
+          username
+        }
+      }
+    )
+
+    if (error !== null) {
+      throw error
+    }
+
+    if (session === null) {
+      throw new Error('Email confirmation is still turned on in the supabase settings, pls fix')
+    }
+
+    if (user === null) {
+      throw new Error('According to the documentation this should never happen, but in the case it does: User obj is null in the supabase.auth.signUp() method')
+    }
+
+    this.client.auth.setAuth(session.access_token)
+    localStorage.setItem('sipapu:access_token', session.access_token)
+  }
+
+  /**
+   * The method for signing in an user for Kokopelli, this method replaces the current access token with the new one
+   * @param email string The email of the user
+   * @param password string The password of the user
+   * @throws {@link Error} if the email or password are incorrect
+   * @throws {@link ApiError} in the case Supabase screws up
+   */
+  async signIn(email: string, password: string): Promise<void> {
+    const { session, error } = await this.client.auth.signIn(
+      {
+        email,
+        password,
+      }
+    )
+
+    if (error !== null) {
+      throw error
+    }
+
+    if (session === null) {
+      throw new Error('Email confirmation is still turned on in the supabase settings, pls fix')
+    }
+
+    this.client.auth.setAuth(session.access_token)
+    localStorage.setItem('sipapu:access_token', session.access_token)
+  }
+
+  static getTokenFromLocalStorage(): string | null {
+    return localStorage.getItem('sipapu:access_token')
   }
 
   /**
@@ -71,93 +139,7 @@ export class Sipapu implements ISipapu {
    */
   async signOut(): Promise<void> {
     await this.client.auth.signOut()
-  }
-
-}
-
-/**
- * The Anonymous Sipapu client.
- * This is the unauthenticated version, it doesn't require an access token to instanciate
- * Usually this is used only for authentication, but can be usefull for the webplayer before a session is set.
- * The query capabilities are limited to the public data.
- * All authentication methods return a {@link Sipapu} client.
- * Then this client is not usefull anymore
- */
-export class AnonymousSipapu implements ISipapu {
-  private client: SupabaseClient
-  Session: Session
-  Playlist: Playlist
-  Song: Song
-
-
-  constructor() {
-    this.client = createClient(settings.supabaseUrl, settings.supabaseKey)
-    this.Session = new Session(this.client, this)
-    this.Playlist = new Playlist(this.client, this)
-    this.Song = new Song(this.client, this)
-  }
-
-  /**
-   * Sign up an user for Kokopelli, and returns a Sipapu instance.
-   * @param email string The email of the user 
-   * @param password string The password of the user 
-   * @param username string The username of the user
-   * @returns A {@link Sipapu} instance
-   * @throws {@link Error} if the email or password are incorrect
-   * @throws {@link ApiError} in the case Supabase screws up
-   */
-  async signUp(email: string, password: string, username: string): Promise<Sipapu> {
-    const { user, session, error } = await this.client.auth.signUp(
-      {
-        email,
-        password,
-      },
-      {
-        data: {
-          username
-        }
-      }
-    )
-
-    if (error !== null) {
-      throw error
-    }
-
-    if (session === null) {
-      throw new Error('Email confirmation is still turned on in the supabase settings, pls fix')
-    }
-
-    if (user === null) {
-      throw new Error('According to the documentation this should never happen, but in the case it does: User obj is null in the supabase.auth.signUp() method')
-    }
-
-    return new Sipapu(session.access_token)
-  }
-
-  /**
-   * The method for signing in an user for Kokopelli, and returns a Sipapu instance.
-   * @param email string The email of the user
-   * @param password string The password of the user
-   * @returns A valid {@link Sipapu} instance
-   * @throws {@link Error} if the email or password are incorrect
-   * @throws {@link ApiError} in the case Supabase screws up
-   */
-  async signIn(email: string, password: string): Promise<Sipapu> {
-    const { session, error } = await this.client.auth.signIn(
-      {
-        email,
-        password,
-      }
-    )
-
-    if (error !== null) {
-      throw error
-    }
-
-    if (session === null) {
-      throw new Error('Email confirmation is still turned on in the supabase settings, pls fix')
-    }
-
-    return new Sipapu(session.access_token)
+    localStorage.removeItem('sipapu:access_token')
+    this.client.auth.setSession('')
   }
 }
